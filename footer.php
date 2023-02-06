c7853eb5f4e9c275c23625f1fc98aa7b440e9591
JennyVicencio <jennyfer.v@rompecabeza.cl>
2023-02-06 18:12:55 -0300

    footer desktop

--- footer.php
+++ footer.php
@@ -11,22 +11,55 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
+	<footer id="footer" class="site-footer">
 
 		<div class="container">
-			<div class="site-info">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'coanil' ) ); ?>">
-					<?php
-					/* translators: %s: CMS name, i.e. WordPress. */
-					printf( esc_html__( 'Proudly powered by %s', 'coanil' ), 'WordPress' );
-					?>
-				</a>
-				<span class="sep"> | </span>
-					<?php
-					/* translators: 1: Theme name, 2: Theme author. */
-					printf( esc_html__( 'Theme: %1$s by %2$s.', 'coanil' ), 'coanil', '<a href="https://www.rompecabeza.cl/">Jennyfer Vicencio</a>' );
-					?>
-			</div><!-- .site-info -->
+			<div class="row py-5">
+				<div class="footer-column logo col-lg-3">
+					<a href="<?php echo get_home_url(); ?>/">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/logos/logo-coanil-white.svg" class="img-fluid" alt="logo Coanil">
+					</a>
+				</div>
+				<div class="footer-column links col-lg-9">
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="footer-column-title">Conócenos</div>
+							<ul>
+								<li><a href="<?php echo get_home_url(); ?>/#">Hablemos</a></li>
+								<li><a href="<?php echo get_home_url(); ?>/trabaja-con-nosotros">Trabaja con nosotros</a></li>
+								<li><a href="<?php echo get_home_url(); ?>/#">Portal trabajador</a></li>
+								<li><a href="<?php echo get_home_url(); ?>/denuncias">Denuncias</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-3">
+							<div class="footer-column-title">Contacto</div>
+							<ul>
+								<li><a href="<?php echo get_home_url(); ?>/programas-sociales">Programas sociales</a></li>
+								<li><a href="<?php echo get_home_url(); ?>/insercion-laboral">Inserción laboral</a></li>
+								<li><a href="<?php echo get_home_url(); ?>/centros-educativos">Centros educativos</a></li>
+								<li><a href="<?php echo get_home_url(); ?>/capacitaciones-otec">Capacitaciones OTEC</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-3">
+							<div class="footer-column-title">Colabora</div>
+							<ul>
+								<li><a href="<?php echo get_home_url(); ?>/#">Dona</a></li>
+								<li><a href="<?php echo get_home_url(); ?>/#">Hazte socio</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-3">
+							<div class="footer-column-title">Nuestros vínculos</div>
+							<ul>
+								<li><a href="<?php echo get_home_url(); ?>/#">Coanil Educa</a></li>
+								<li><a href="<?php echo get_home_url(); ?>/#">coanil Servicios</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer-copyright py-4">
+				<span>(c) Copyright <a href="mailto:comunicaciones@coanil.cl">comunicaciones@coanil.cl</a> | Derechos Reservados | Coanil Sede Central: Julio Prado 1761, Ñuñoa, Santiago</span>
+			</div>
 		</div>
 		
 	</footer><!-- #colophon -->
